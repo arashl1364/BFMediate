f4fe7a41422870fbc1b0c23eb8fced590eb89d3a
jasperiankl <32761724+jasperiankl@users.noreply.github.com>
2020-05-01 16:29:27 +0200

    Compile Fail 2
    Failure for rordprobitGibbs_me_multi_merr.cpp

---
+++ R/rordprobitGibbs_me_multi_merr.R
@@ -0,0 +1,206 @@
+#' qwerqw
+#'
+#' @param Data qwerqwer
+#' @param Prior qwerwer
+#' @param Mcmc qwererrr
+#'
+#' @return
+#' @export
+#'
+#' @import bayesm
+rordprobitGibbs_me_multi_merr_cpp=function(Data,Prior,Mcmc){
+  #
+  # revision history:
+  #   3/07  Hsiu-Wen Liu
+  #   3/07  fixed naming of dstardraw rossi
+  #
+  # purpose:
+  #   draw from posterior for ordered probit using Gibbs Sampler
+  #   and metropolis RW
+  #
+  # Arguments:
+  #   Data - list of X,y,k
+  #     X is nobs x nvar, y is nobs vector of 1,2,.,k (ordinal variable)
+  #   Prior - list of A, betabar
+  #     A is nvar x nvar prior preci matrix
+  #     betabar is nvar x 1 prior mean
+  #     Ad is ndstar x ndstar prior preci matrix of dstar (ncut is number of cut-offs being estimated)
+  #     dstarbar is ndstar x 1 prior mean of dstar
+  #   Mcmc
+  #     R is number of draws
+  #     keep is thinning parameter
+  #     nprint - print estimated time remaining on every nprint'th draw
+  #     s is scale parameter of random work Metropolis
+  #
+  # Output:
+  #   list of betadraws and cutdraws
+  #
+  # Model:
+  #    z=Xbeta + e  < 0  e ~N(0,1)
+  #    y=1,..,k, if z~c(c[k], c[k+1])
+  #
+  #    cutoffs = c[1],..,c[k+1]
+  #    dstar = dstar[1],dstar[k-2]
+  #    set c[1]=-100, c[2]=0, ...,c[k+1]=100
+  #
+  #    c[3]=exp(dstar[1]),c[4]=c[3]+exp(dstar[2]),...,
+  #    c[k]=c[k-1]+exp(datsr[k-2])
+  #
+  # Note: 1. length of dstar = length of cutoffs - 3
+  #       2. Be careful in assessing prior parameter, Ad.  .1 is too small for many applications.
+  #
+  # Prior: beta ~ N(betabar,A^-1)
+  #        dstar ~ N(dstarbar, Ad^-1)
+  #
+  #
+  # ----------------------------------------------------------------------
+  # Rcpp::sourceCpp('rordprobitGibbs_me_multi_merr.cpp')
+  # define functions needed
+  #  dstartoc is a fuction to transfer dstar to its cut-off value
+
+  dstartoc=function(dstar) {c(-100, 0, cumsum(exp(dstar)), 100)}
+
+  # compute conditional likelihood of data given cut-offs
+  #
+  lldstar=function(dstar,y,mu){
+    gamma=dstartoc(dstar)
+    arg = pnorm(gamma[y+1]-mu)-pnorm(gamma[y]-mu)
+    epsilon=1.0e-50
+    arg=ifelse(arg < epsilon,epsilon,arg)
+    return(sum(log(arg)))
+  }
+  #
+  # ----------------------------------------------------------------------
+  #
+  # check arguments
+  #
+  if(missing(Data)) {pandterm("Requires Data argument -- list of y and X")}
+  if(is.null(Data$X)) {pandterm("Requires Data element X")}
+  X=Data$X
+  if(is.null(Data$y)) {pandterm("Requires Data element y")}
+  y=Data$y
+  if(is.null(Data$k)) {pandterm("Requires Data element k")}
+  k=Data$k
+  if(is.null(Data$Y_ind)) {pandterm("Requires Data element Y_ind")}
+  Y_ind=Data$Y_ind
+
+  nvar=ncol(X)
+  nobs=dim(y)[1]
+  ndstar = k-2         # number of dstar being estimated
+  ncuts = k+1          # number of cut-offs (including zero and two ends)
+  ncut = ncuts-3       # number of cut-offs being estimated c[1]=-100, c[2]=0, c[k+1]=100
+
+
+  cutoff_Y_init = Data$cutoff_Y_init
+  Y_tilde_init = Data$Y_tilde_init
+  ssq_y_tilde_init = Data$ssq_y_tilde_init
+  beta_2_init = Data$beta_2_init
+  Y_init = Data$Y_init
+  beta_tilde_init = Data$beta_tilde_init
+
+  #
+  # check data for validity
+  #
+  if(dim(y)[1] != nrow(X) ) {pandterm("y and X not of same row dim")}
+  if(  sum(unique(y[,1]) %in% (1:k) ) < length(unique(y[,1])) )   #Tests only y_star1 but I really don't care! :D
+  {pandterm("some value of y is not vaild")}
+
+  #
+  # check for Prior
+  #
+  if(missing(Prior))
+  { betabar=c(rep(0,nvar)); A=.01*diag(nvar); Ad=diag(ndstar); dstarbar=c(rep(0,ndstar))}
+  else
+  {
+    if(is.null(Prior$betabar)) {betabar=c(rep(0,nvar))}
+    else {betabar=Prior$betabar}
+    if(is.null(Prior$A)) {A=.01*diag(nvar)}
+    else {A=Prior$A}
+    if(is.null(Prior$Ad)) {Ad=diag(ndstar)}
+    else {Ad=Prior$Ad}
+    if(is.null(Prior$dstarbar)) {dstarbar=c(rep(0,ndstar))}
+    else {dstarbar=Prior$dstarbar}
+  }
+  #
+  # check dimensions of Priors
+  #
+
+  if(ncol(A) != nrow(A) || ncol(A) != nvar || nrow(A) != nvar)
+  {pandterm(paste("bad dimensions for A",dim(A)))}
+  if(length(betabar) != nvar)
+  {pandterm(paste("betabar wrong length, length= ",length(betabar)))}
+  if(ncol(Ad) != nrow(Ad) || ncol(Ad) != ndstar || nrow(Ad) != ndstar)
+  {pandterm(paste("bad dimensions for Ad",dim(Ad)))}
+  if(length(dstarbar) != ndstar)
+  {pandterm(paste("dstarbar wrong length, length= ",length(dstarbar)))}
+
+  #
+  # check MCMC argument
+  #
+  if(missing(Mcmc)) {pandterm("requires Mcmc argument")}
+  else
+  {
+    if(is.null(Mcmc$R))
+    {pandterm("requires Mcmc element R")} else {R=Mcmc$R}
+    if(is.null(Mcmc$keep)) {keep=1} else {keep=Mcmc$keep}
+    if(is.null(Mcmc$nprint)) {nprint=100} else {nprint=Mcmc$nprint}
+    if(nprint<0) {pandterm('nprint must be an integer greater than or equal to 0')}
+    if(is.null(Mcmc$s)) {s=2.38/sqrt(ndstar)} else {s=Mcmc$s}  #2.38 is the RRscaling
+  }
+  #
+  # print out problem
+  #
+  cat(" ", fill=TRUE)
+  cat("Starting Gibbs Sampler for Ordered Probit Model",fill=TRUE)
+  cat("   with ",nobs,"observations",fill=TRUE)
+  cat(" ", fill=TRUE)
+  cat("Table of y values",fill=TRUE)
+  for(i in 1:Y_ind) print(table(y[,i]))
+  cat(" ",fill=TRUE)
+  cat("Prior Parms: ",fill=TRUE)
+  cat("betabar",fill=TRUE)
+  print(betabar)
+  cat(" ", fill=TRUE)
+  cat("A",fill=TRUE)
+  print(A)
+  cat(" ", fill=TRUE)
+  cat("dstarbar",fill=TRUE)
+  print(dstarbar)
+  cat(" ", fill=TRUE)
+  cat("Ad",fill=TRUE)
+  print(Ad)
+  cat(" ", fill=TRUE)
+  cat("MCMC parms: ",fill=TRUE)
+  cat("R= ",R," keep= ",keep," nprint= ",nprint,"s= ",s, fill=TRUE)
+  cat(" ",fill=TRUE)
+
+  # use (-Hessian+Ad)^(-1) evaluated at betahat as the basis of the
+  # covariance matrix for the random walk Metropolis increments
+
+  betahat = chol2inv(chol(crossprod(X,X)))%*% crossprod(X,rowMeans(y))    ###betahat is computed based on the average of y_stars
+  dstarini = c(cumsum(c( rep(0.1, ndstar))))     # set initial value for dstar
+  dstarout = optim(dstarini, lldstar, method = "BFGS", hessian=T,
+                   control = list(fnscale = -1,maxit=500,
+                                  reltol = 1e-06, trace=0), mu=X%*%betahat, y=rowMeans(y))
+  inc.root=chol(chol2inv(chol((-dstarout$hessian+Ad))))  # chol((H+Ad)^-1)
+
+  ###################################################################
+  # Keunwoo Kim
+  # 08/20/2014
+  ###################################################################
+  draws=rordprobitGibbs_me_multi_merr_cpp_loop(y,X,k,A,betabar,Ad,s,inc.root,dstarbar,betahat,
+                                          Y_ind,
+                                          R,keep,nprint)
+                                          # cutoff_Y_init, Y_tilde_init, beta_tilde_init, ssq_y_tilde_init, beta_2_init, Y_init)
+  ###################################################################
+
+  draws$cutdraw=draws$cutdraw[,2:k,]
+  # attributes(draws$cutdraw)$class="bayesm.mat"
+  # attributes(draws$betadraw)$class="bayesm.mat"
+  # attributes(draws$dstardraw)$class="bayesm.mat"
+  # attributes(draws$cutdraw)$mcpar=c(1,R,keep)
+  # attributes(draws$betadraw)$mcpar=c(1,R,keep)
+  # attributes(draws$dstardraw)$mcpar=c(1,R,keep)
+
+  return(draws)
+}
--- src/cpp_functions_share.cpp
+++ src/cpp_functions_share.cpp
@@ -5,30 +5,29 @@ using namespace arma; // use the Armadillo library for matrix computations
 using namespace Rcpp;
 
 
-//EXTRA FUNCTIONS SPECIFIC TO THE MAIN FUNCTION--------------------------------------------
 // [[Rcpp::export]]
 List runiregGibbs_betafix(arma::vec const& y, arma::mat const& X, arma::vec const& betabar, arma::mat const& A, double nu, double ssq,
                           double sigmasq, int R, int keep, int nprint, int betafix) {
-  
+
   // Keunwoo Kim 09/09/2014
-  
+
   // Purpose: perform iid draws from posterior of regression model using conjugate prior
-  
+
   // Arguments:
   //  y,X
   //  betabar,A      prior mean, prior precision
   //  nu, ssq        prior on sigmasq
   //  R number of draws
   //  keep thinning parameter
-  
+
   // Output: list of beta, sigmasq
-  
+
   // Model:
   //  y = Xbeta + e  e ~N(0,sigmasq)
   //  y is n x 1
   //  X is n x k
   //  beta is k x 1 vector of coefficients
-  
+
   // Prior:
   //  beta ~ N(betabar,sigmasq*A^-1)
   //  sigmasq ~ (nu*ssq)/chisq_nu
@@ -37,24 +36,24 @@ List runiregGibbs_betafix(arma::vec const& y, arma::mat const& X, arma::vec cons
   double s;
   arma::mat RA, W, IR;
   arma::vec z, btilde,beta;
-  
+
   int nvar = X.n_cols;
   int nobs = y.size();
-  
+
   arma::vec sigmasqdraw(R/keep);
   arma::mat betadraw(R/keep, nvar);
-  
+
   arma::mat XpX = trans(X)*X;
   arma::vec Xpy = trans(X)*y;
-  
+
   arma::vec Abetabar = A*betabar;
-  
+
   // if (nprint>0) startMcmcTimer();
-  
+
   for (int rep=0; rep<R; rep++){
-    
+
     if(betafix==1) beta <<0<<1;
-    
+
     else{
       //first draw beta | sigmasq
       IR = solve(trimatu(chol(XpX/sigmasq+A)), eye(nvar,nvar)); //trimatu interprets the matrix as upper triangular and makes solve more efficient
@@ -62,23 +61,23 @@ List runiregGibbs_betafix(arma::vec const& y, arma::mat const& X, arma::vec cons
       btilde = (IR*trans(IR)) * (Xpy/sigmasq+Abetabar);
       beta = btilde + IR*vec(rnorm(nvar));
     }
-    
+
     //now draw sigmasq | beta
     s = sum(square(y-X*beta));
     sigmasq = (nu*ssq+s) / rchisq(1,nu+nobs)[0]; //rchisq returns a vectorized object, so using [0] allows for the conversion to double
-    
+
     //print time to completion and draw # every nprint'th draw
     // if (nprint>0) if ((rep+1)%nprint==0) infoMcmcTimer(rep, R);
-    
+
     if((rep+1)%keep==0){
       mkeep = (rep+1)/keep;
       betadraw(mkeep-1, span::all) = trans(beta);
       sigmasqdraw[mkeep-1] = sigmasq;
     }
   }
-  
+
   // if (nprint>0) endMcmcTimer();
-  
+
   return List::create(
     Named("betadraw") = betadraw,
     Named("sigmasqdraw") = NumericVector(sigmasqdraw.begin(),sigmasqdraw.end()));
@@ -88,15 +87,15 @@ List runiregGibbs_betafix(arma::vec const& y, arma::mat const& X, arma::vec cons
 
 // [[Rcpp::export]]
 List breg_me(arma::vec const& y, arma::mat const& X, arma::vec const& betabar, arma::mat const& A) {
-  
+
   // Purpose: compute the posterior moments for linear regression, sigmasq=1.0
-  
+
   // Output: draw from posterior
-  
+
   // Model: y = Xbeta + e  e ~ N(0,I)
-  
+
   // Prior:  beta ~ N(betabar,A^-1)
-  
+
   int k = betabar.size();
   arma::mat RA = chol(A);
   // printf("98");
@@ -104,7 +103,7 @@ List breg_me(arma::vec const& y, arma::mat const& X, arma::vec const& betabar, a
   arma::vec z = arma::join_cols(y, RA*betabar);  //z is the v matrix in Rossi slides
   arma::mat IR = solve(trimatu(chol(trans(W)*W)), eye(k,k)); //trimatu interprets the matrix as upper triangular and makes solve more efficient
   // printf("102");
-  
+
   return List::create(
     Named("mubeta") = (IR*trans(IR))*(trans(W)*z),
     Named("IR")  = IR);
@@ -115,16 +114,16 @@ List breg_me(arma::vec const& y, arma::mat const& X, arma::vec const& betabar, a
 // [[Rcpp::export]]
 NumericVector rtrun(NumericVector const& mu, NumericVector const& sigma,
                     NumericVector const& a, NumericVector const& b){
-  
+
   // Wayne Taylor 9/7/2014
-  
+
   // function to draw from univariate truncated norm
   // a is vector of lower bounds for truncation
   // b is vector of upper bounds for truncation
-  
+
   NumericVector FA = pnorm((a-mu)/sigma);
   NumericVector FB = pnorm((b-mu)/sigma);
-  
+
   return(mu+sigma*qnorm(runif(mu.size())*(FB-FA)+FA));
 }
 
@@ -138,7 +137,7 @@ arma::vec dstartoc(arma::vec const& dstar){
   c[1] = 0;
   c(span(2,ndstar+1)) = cumsum(exp(dstar));
   c[ndstar+2] = 100;
-  
+
   return (c);
 }
 
@@ -147,7 +146,7 @@ arma::vec dstartoc(arma::vec const& dstar){
 // [[Rcpp::export]]
 double lldstar(arma::vec const& dstar, arma::vec const& y, vec const& mu, double ssq_y_tilde){       //y is y*, mu (= z) is y, ssq_y_tilde is the variance of indicator latent variables
   arma::vec gamma = dstartoc(dstar);
-  
+
   double sigma_y_tilde = sqrt(ssq_y_tilde);
   int ny = y.size();
   NumericVector gamma1(ny);
@@ -173,7 +172,7 @@ double lldstar(arma::vec const& dstar, arma::vec const& y, vec const& mu, double
 // [[Rcpp::export]]
 List dstarRwMetrop(arma::vec const& y, arma::vec const& mu, arma::vec const& olddstar, double s, arma::mat const& inc_root,
                    arma::vec const& dstarbar, arma::mat const& rootdi, int ncut, double ssq_y_tilde){
-  
+
   // function to execute rw metropolis for the dstar
   // y is n vector with element = 1,...,j
   // X is n x k matrix of x values
@@ -186,25 +185,25 @@ List dstarRwMetrop(arma::vec const& y, arma::vec const& mu, arma::vec const& old
   // int stay = 0;
   double unif;
   arma::vec dstardraw;
-  
+
   arma::vec dstarc = olddstar + s*trans(inc_root)*vec(rnorm(ncut));
   double oldll = lldstar(olddstar, y, mu, ssq_y_tilde);
   double cll = lldstar(dstarc, y, mu, ssq_y_tilde);
   double clpost = cll + lndMvn(dstarc, dstarbar, rootdi);
   double ldiff = clpost - oldll - lndMvn(olddstar, dstarbar, rootdi);
   double alpha = exp(ldiff);
-  
+
   if (alpha>1){
     alpha = 1.0;
   }
-  
+
   if (alpha<1){
     unif = runif(1)[0]; //runif returns a NumericVector, so using [0] allows for conversion to double by extracting the first element
   }
   else{
     unif = 0;
   }
-  
+
   if (unif<=alpha){
     dstardraw = dstarc;
     oldll = cll;
@@ -213,7 +212,7 @@ List dstarRwMetrop(arma::vec const& y, arma::vec const& mu, arma::vec const& old
     dstardraw = olddstar;
     // stay = 1;
   }
-  
+
   return List::create(
     Named("dstardraw") = dstardraw,
     Named("oldll") = oldll
@@ -225,7 +224,7 @@ List dstarRwMetrop(arma::vec const& y, arma::vec const& mu, arma::vec const& old
 // [[Rcpp::export]]
 List dstarRwMetrop_1(arma::vec const& y, arma::vec const& mu, arma::vec const& olddstar, double s, arma::mat const& inc_root,
                      arma::vec const& dstarbar, arma::mat const& rootdi, int ncut, double ssq_y_tilde){
-  
+
   // function to execute rw metropolis for the dstar
   // y is n vector with element = 1,...,j
   // X is n x k matrix of x values
@@ -234,35 +233,35 @@ List dstarRwMetrop_1(arma::vec const& y, arma::vec const& mu, arma::vec const& o
   //  inc.root, rootdi are upper triangular
   //  this means that we are using the UL decomp of Sigma^-1 for prior
   // olddstar is the current
-  
+
   double unif;
   arma::vec dstardraw;
-  
+
   arma::vec dstarc = olddstar + s*trans(inc_root)*vec(rnorm(ncut));
   double oldll = lldstar(olddstar, y, mu, ssq_y_tilde);
   double cll = lldstar(dstarc, y, mu, ssq_y_tilde);
   double clpost = cll + lndMvn(dstarc, dstarbar, rootdi);
   double ldiff = clpost - oldll - lndMvn(olddstar, dstarbar, rootdi);
   double alpha = exp(ldiff);
-  
+
   if (alpha>1){
     alpha = 1.0;
   }
-  
+
   if (alpha<1){
     unif = runif(1)[0]; //runif returns a NumericVector, so using [0] allows for conversion to double by extracting the first element
   }
   else{
     unif = 0;
   }
-  
+
   if (unif<=alpha){
     dstardraw = dstarc;
   }
   else{
     dstardraw = olddstar;
   }
-  
+
   return List::create(
     Named("dstardraw") = dstardraw
   );
@@ -271,12 +270,12 @@ List dstarRwMetrop_1(arma::vec const& y, arma::vec const& mu, arma::vec const& o
 
 // [[Rcpp::export]]
 List breg2(arma::mat const& root, arma::mat const& X, arma::vec const& y, arma::vec const& Abetabar) {
-  
+
   // Arash Laghaie 10/20/2018
   // Equivalent to breg1 and additionally stores the beta moments
-  
+
   arma::mat cov = trans(root)*root;
-  
+
   return List::create(
     Named("beta") = cov*(trans(X)*y+Abetabar) + trans(root)*vec(rnorm(root.n_cols)),
     Named("mubeta") = cov*(trans(X)*y+Abetabar),
@@ -312,13 +311,13 @@ double half_norm_rs(double a, double b)
 double unif_rs(double a, double b)
 {
   double xstar, logphixstar, x, logu;
-  
+
   // Find the argmax (b is always >= 0)
   // This works because we want to sample from N(0,1)
   if(a <= 0.0) xstar = 0.0;
   else xstar = a;
   logphixstar = R::dnorm(xstar, 0.0, 1.0, 1.0);
-  
+
   x = R::runif(a, b);
   logu = log(R::runif(0.0, 1.0));
   while( logu > (R::dnorm(x, 0.0, 1.0,1.0) - logphixstar))
@@ -333,16 +332,16 @@ double unif_rs(double a, double b)
 double exp_rs(double a, double b)
 {
   double  z, u, rate;
-  
+
   //  Rprintf("in exp_rs");
   rate = 1/a;
   //1/a
-  
+
   // Generate a proposal on (0, b-a)
   z = R::rexp(rate);
   while(z > (b-a)) z = R::rexp(rate);
   u = R::runif(0.0, 1.0);
-  
+
   while( log(u) > (-0.5*z*z))
   {
     z = R::rexp(rate);
@@ -361,12 +360,12 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
   arma::vec out(nobs);
   double logt1 = log(0.150), logt2 = log(2.18), t3 = 0.725;
   double a,b, z, tmp, lograt;
-  
+
   int  change;
-  
+
   // #pragma omp parallel for schedule(dynamic)
   for(int i=0;i<nobs;i++) {
-    
+
     a = (lower(i) - mus(i))/sigmas(i);
     b = (upper(i) - mus(i))/sigmas(i);
     change=0;
@@ -379,7 +378,7 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
         a = -b;
         b = R_PosInf;
       }
-      
+
       // The two possibilities for this scenario
       if(a <= 0.45) z = norm_rs(a, b);
       else z = exp_rs(a, b);
@@ -395,7 +394,7 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
       }
       else z = unif_rs(a,b);
     }
-    
+
     // Third scenario
     else
     {
@@ -403,7 +402,7 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
       {
         tmp = b; b = -a; a = -tmp; change = 1;
       }
-      
+
       lograt = R::dnorm(a, 0.0, 1.0, 1.0) - R::dnorm(b, 0.0, 1.0, 1.0);
       if(lograt <= logt2) z = unif_rs(a,b);
       else if((lograt > logt1) && (a < t3)) z = half_norm_rs(a,b);
@@ -412,7 +411,7 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
     }
     out(i)=sigmas(i)*z + mus(i);
   }
-  
+
   return(out);
 }
 
@@ -424,13 +423,13 @@ arma::vec rtnm(arma::vec mus, arma::vec sigmas, arma::vec lower, arma::vec upper
 //The functions below are used to print the output from MCMC draws for many of the bayesm functions
 // [[Rcpp::export]]
 arma::vec rtrunVec(arma::vec const& mu,arma::vec const& sigma, arma::vec const& a, arma::vec const& b){
-  
+
   // Keunwoo Kim 06/20/2014
-  
+
   //function to draw from univariate truncated norm
   //a is vector of lower bounds for truncation
   //b is vector of upper bounds for truncation
-  
+
   int n = mu.size();
   arma::vec FA(n);
   arma::vec FB(n);
@@ -440,7 +439,7 @@ arma::vec rtrunVec(arma::vec const& mu,arma::vec const& sigma, arma::vec const& 
     FB[i] = R::pnorm((b[i]-mu[i])/sigma[i],0,1,1,0);
     out[i] = mu[i]+sigma[i]*R::qnorm(R::runif(0,1)*(FB[i]-FA[i])+FA[i],0,1,1,0);
   }
-  
+
   return(out);
 }
 
@@ -459,7 +458,7 @@ void startMcmcTimer() {
 void infoMcmcTimer(int rep, int R) {
   time_t ctime = time(NULL);
   char buf[32];
-  
+
   double timetoend = difftime(ctime, itime) / 60.0 * (R - rep - 1) / (rep+1);
   sprintf(buf, " %d (%.1f)\n", rep+1, timetoend);
   Rcout <<  buf;
@@ -469,50 +468,50 @@ void infoMcmcTimer(int rep, int R) {
 void endMcmcTimer() {
   time_t ctime = time(NULL);
   char buf[32];
-  
+
   sprintf(buf, " Total Time Elapsed: %.2f \n", difftime(ctime, itime) / 60.0);
   Rcout << buf;
-  
+
   itime = 0;
 }
 
 // [[Rcpp::export]]
 arma::vec breg1(arma::mat const& root, arma::mat const& X, arma::vec const& y, arma::vec const& Abetabar) {
-  
+
   // Keunwoo Kim 06/20/2014
-  
+
   // Purpose: draw from posterior for linear regression, sigmasq=1.0
-  
+
   // Arguments:
   //  root = chol((X'X+A)^-1)
   //  Abetabar = A*betabar
-  
+
   // Output: draw from posterior
-  
+
   // Model: y = Xbeta + e  e ~ N(0,I)
-  
+
   // Prior: beta ~ N(betabar,A^-1)
-  
+
   arma::mat cov = trans(root)*root;
-  
+
   return (cov*(trans(X)*y+Abetabar) + trans(root)*vec(rnorm(root.n_cols)));
 }
 
 
 // [[Rcpp::export]]
 double lndMvn(arma::vec const& x, arma::vec const& mu, arma::mat const& rooti){
-  
+
   //Wayne Taylor 9/7/2014
-  
+
   // function to evaluate log of MV Normal density with  mean mu, var Sigma
   // Sigma=t(root)%*%root   (root is upper tri cholesky root)
   // Sigma^-1=rooti%*%t(rooti)
   // rooti is in the inverse of upper triangular chol root of sigma
   //          note: this is the UL decomp of sigmai not LU!
   //                Sigma=root'root   root=inv(rooti)
-  
+
   arma::vec z = vectorise(trans(rooti)*(x-mu));
-  
+
   return((-(x.size()/2.0)*log(2*M_PI) -.5*(trans(z)*z) + sum(log(diagvec(rooti))))[0]);
 }
 
