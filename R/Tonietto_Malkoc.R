79644b55c296d8db58665fa877214814a807cca6
arashl1364 <arash.laghaie@gmail.com>
2022-07-06 16:19:34 +0200

    Edit dataset help files

--- R/Tonietto_Malkoc.R
+++ R/Tonietto_Malkoc.R
@@ -15,6 +15,12 @@
 #' \item{...$Effortful}{ Work construal indicator 2 (m_2*_2) }
 #' \item{...$WorkLike}{ Work construal indicator 3 (m_2*_3) }
 #' \item{...$Scale_Work}{ Work construal composite measure }
+#' \item{...$Excited}{ Anticipation utility indicator 1 (y*_1) }
+#' \item{...$Thrilled}{ Anticipation utility indicator 2 (y*_2) }
+#' \item{...$LookingForward}{ Anticipation utility indicator 3 (y*_3) }
+#' \item{...$ResentfulR}{ Anticipation utility indicator 4 (y*_4) }
+#' \item{...$UnenthusiasticR}{ Anticipation utility indicator 5 (y*_5) }
+#' \item{...$ReluctantR}{ Anticipation utility indicator 6 (y*_6) }
 #' \item{...$Scale_Anticipation_Utility}{  Anticipation utility composite measure }
 #' }
 #' 
@@ -24,12 +30,20 @@
 #' 
 #' data(Tonietto_Malkoc)
 #' 
+#' # Finding indices of observations that contain missing values
+#' which(is.na(Tonietto_Malkoc),arr.ind = T)
+#' 
+#' # Removing (the two) observations that contain missing values from the dataset
+#' Tonietto_Malkoc = Tonietto_Malkoc[-c(56,96),]
+#' 
 #' Data = NULL
 #' Data$X = ifelse(Tonietto_Malkoc$Condition=="Impromptu",2,-1)    # C1 contrast coding of X (based on the paper)
 #' Data$M1 = Tonietto_Malkoc$Scale_Freeflowing
 #' Data$M2 = Tonietto_Malkoc$Scale_Work
-#' Data$m_tilde = cbind(Tonietto_Data$Chore,Tonietto_Data$Effortful,Tonietto_Data$WorkLike,8-Tonietto_Data$Flexible,8-Tonietto_Data$Freeflowing)
+#' Data$m_tilde = cbind(Tonietto_Malkoc$Chore,Tonietto_Malkoc$Effortful,Tonietto_Malkoc$WorkLike,8-Tonietto_Malkoc$Flexible,8-Tonietto_Malkoc$Freeflowing)
+#' Data$y_tilde = cbind(Tonietto_Malkoc$Excited, Tonietto_Malkoc$Thrilled, Tonietto_Malkoc$LookingForward,Tonietto_Malkoc$ResentfulR, Tonietto_Malkoc$UnenthusiasticR, Tonietto_Malkoc$ReluctantR)
 #' Data$Y = Tonietto_Malkoc$Scale_Anticipation_Utility
+#' 
 #' 
 #' # Saving the dataset to use in the Shiny app (https://bfmediate.shinyapps.io/bfmediate_app/)
 #' save(Data,file = "~/Tonietto_Malkoc.rda")    # the file path can be changed by replacing ~
@@ -96,10 +110,10 @@
 #' Data5$X = Data$X
 #' # using the indicators of (reverse) Freeflowing and work construal as indicators for a common latent mediator
 #' Data5$m_tilde = Data$m_tilde
-#' Data5$Y = Data$Y
+#' Data5$y_tilde = Data$y_tilde
 #' 
 #' # Computing Bayes factor for the new latent variable model
-#' outnew = Mediate(Data = Data5, Model = 'MCat', Prior = list(A_M = A_M, A_Y = A_Y),R=10000, burnin = 2000)
+#' outnew = Mediate(Data = Data5, Model = 'MYCat', Prior = list(A_M = A_M, A_Y = A_Y),R=10000, burnin = 2000)
 #' outnew$BF
 #' outnew$evidence
 "Tonietto_Malkoc"
